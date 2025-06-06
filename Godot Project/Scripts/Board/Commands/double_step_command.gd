779d183f4e3b2ad0d83505fbb0b628f03e16bdd0
MagikBased <124455610+MagikBased@users.noreply.github.com>
2025-06-05 21:42:21 -0500

    Fix special move command inheritance

---
+++ Godot Project/Scripts/Board/Commands/double_step_command.gd
@@ -0,0 +1,8 @@
+extends SpecialMoveCommand
+class_name DoubleStepCommand
+
+func execute(context: LogicContext) -> void:
+	if context.piece_instance == null:
+		return
+	var position: Vector2i = context.additional_data.get("highlight_position", Vector2i.ZERO)
+	context.piece_instance._on_move_piece(position)
