31ed67ec01022291e91d943d42f68033e5d9667d
MagikBased <124455610+MagikBased@users.noreply.github.com>
2025-06-05 21:48:08 -0500

    Revert "Added support for creating special move commands to the square highlights."
    This reverts commit 3bb5ae747b1f9be3528055fdf774b37fe99d77d8.

--- Godot Project/Scripts/Board/square_highlight.gd
+++ Godot Project/Scripts/Board/square_highlight.gd
@@ -7,7 +7,6 @@ var parent_node: Node2D
 signal move_piece(position: Vector2i)
 signal drop_piece(position: Vector2i)
 
-@export var special_command: SpecialMoveCommand
 
 func _ready() -> void:
 	parent_node = get_parent() as BaseGamePiece
@@ -15,14 +14,5 @@ func _ready() -> void:
 
 func _input(event) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed() and get_rect().has_point(to_local(event.position)):
-		if special_command != null:
-			var context := LogicContext.new()
-			context.piece_instance = parent_node
-			context.game_state = parent_node.game_manager
-			context.additional_data = { "highlight_position": current_position }
-			special_command.execute(context)
-		else:
-			if is_dropping:
-				emit_signal("drop_piece", current_position)
-			else:
-				emit_signal("move_piece", current_position)
+		emit_signal("drop_piece", current_position)
+		emit_signal("move_piece", current_position)
--- Godot Project/Scripts/ResourceScripts/MovementResourceScripts/LogicBlocks/LogicResourceScripts/LogicEffects/special_move_command.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name SpecialMoveCommand
-
-func execute(context: LogicContext) -> void:
-	push_error("execute() not implemented in SpecialMoveCommand subclass", context)
