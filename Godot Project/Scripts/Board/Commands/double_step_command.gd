78fda8a1bf748f81a29d3700f3df801108633a71
MagikBased <124455610+MagikBased@users.noreply.github.com>
2025-06-05 22:00:00 -0500

    Revert "Merge pull request #5 from MagikBased/codex/extend-special-moves-to-square-highlights"
    This reverts commit e659425e6f776a8de839b1d22b473b8a4511c6c2, reversing
    changes made to 3bb5ae747b1f9be3528055fdf774b37fe99d77d8.

--- Godot Project/Scripts/Board/Commands/double_step_command.gd
+++
@@ -1,8 +0,0 @@
-extends "res://Scripts/ResourceScripts/MovementResourceScripts/LogicBlocks/LogicResourceScripts/LogicEffects/special_move_command.gd"
-class_name DoubleStepCommand
-
-func execute(context: LogicContext) -> void:
-	if context.piece_instance == null:
-		return
-	var position: Vector2i = context.additional_data.get("highlight_position", Vector2i.ZERO)
-	context.piece_instance._on_move_piece(position)
